aa18a796cae5217a3b802589af002c4bf161aa27
Florian Moreau <florian.moreau@epitech.eu>
2017-11-11 18:36:41 +0100

    new repo rdy

--- cardGames/cardGames/Program.cs
+++ cardGames/cardGames/Program.cs
@@ -15,22 +15,6 @@ namespace cardGames
 
             string  IP = src.Split(':').First();
             int     Port = int.Parse(src.Split(':').Last());
-
-            int loopCounter = 1;
-            while (true)
-            {
-
-                //Send the message in a single line
-                NetworkComms.SendObject("Message", IP, Port, messageToSend);
-
-                //Check if user wants to go around the loop
-                Console.WriteLine("\nPress q to quit or any other key to send another message.");
-                if (Console.ReadKey(true).Key == ConsoleKey.Q) break;
-                else loopCounter++;
-            }
-
-            //We have used comms so we make sure to call shutdown
-            NetworkComms.Shutdown();
         }
     }
 }
