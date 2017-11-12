00c04d8314ee4361e90bd2cd9cc87034be4dcada
Florian Moreau <florian.moreau@epitech.eu>
2017-11-12 16:38:37 +0100

    add deck object / command basic handler in client / some commands in protocol

---
+++ cardGames/ConsoleApp1/Deck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardGames_Server
+{
+    class Deck
+    {
+        public enum cards { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
+        private List<int> deck = new List<int>();
+        private int availablecards;
+        private Random rnd;
+
+        public Deck()
+        {
+            availablecards = 52;
+            for (int i = 0; i < 13; ++i)
+                deck.Add(4);
+            rnd = new Random();
+        }
+
+        public cards GetNextCard()
+        {
+            int dest = rnd.Next(1, 13);
+
+            while (deck[dest] <= 0) { dest = rnd.Next(1, 13); }
+            deck[dest]--;
+            availablecards--;
+            return ((cards)dest);
+        }
+    }
+}
