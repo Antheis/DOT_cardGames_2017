e86724ed0196e38a012888218bc11cf18fe61ddf
Florian Moreau <florian.moreau@epitech.eu>
2017-11-12 17:51:42 +0100

    add bataille / BJ function client

--- cardGames/cardGames/Client.cs
+++ cardGames/cardGames/Client.cs
@@ -20,16 +20,17 @@ namespace cardGame_Client
         private DataSerializer dataSerializer { get; set; }
         private List<DataProcessor> dataProcessors { get; set; }
         private Dictionary<string, string> dataProcessorOptions { get; set; }
-        private string  IP;
-        private int     Port;
-        private string  ID;
+        private string IP;
+        private int Port;
+        private string ID;
         //private List<string> Bataille_available_actions = new List<string>(new string[] { });
         //private List<string> BJ_available_actions = new List<string>(new string[] { });
-        private List<string> menu_available_actions = new List<string>(new string[] { "'BJ' to play blackjack", "'Bataille' to play bataille", "'help' to get available commands", "'quit' to close the client" });
+        private List<string> menu_available_actions = new List<string>(new string[] { "1 -'BJ' to play blackjack", "2 -'BT' to play bataille", "3 -'help' to get available commands", "4 -'quit' to close the client" });
+        private enum State { BlackJack, Bataille, Menu };
 
         public Client()
         {
-            string src;
+            /*string src;
 
             Console.WriteLine("Please enter the server IP and port in the format IP:Port");
             src = Console.ReadLine();
@@ -42,27 +43,58 @@ namespace cardGame_Client
 
             dataSerializer = DPSManager.GetDataSerializer<ProtobufSerializer>();
             dataProcessors = new List<DataProcessor>();
-            dataProcessorOptions = new Dictionary<string, string>();
+            dataProcessorOptions = new Dictionary<string, string>();*/
+        }
+
+        private void Bataille()
+        {
+            Console.WriteLine("Bataille");
+            Console.WriteLine("\n");
+        }
+
+        private void BlackJack()
+        {
+            Console.WriteLine("BlackJack");
+            Console.WriteLine("\n");
+        }
+
+        private void printhelp()
+        {
+            foreach (string T in menu_available_actions) { Console.WriteLine(T); }
+            Console.WriteLine("\n");
         }
 
         public void start()
         {
-            Connection TCPconn = TCPConnection.GetConnection(new ConnectionInfo(IP, Port));
+            /*Connection TCPconn = TCPConnection.GetConnection(new ConnectionInfo(IP, Port));
             NetworkComms.DefaultSendReceiveOptions = new SendReceiveOptions(dataSerializer, dataProcessors, dataProcessorOptions);
             NetworkComms.AppendGlobalIncomingPacketHandler<string>("Message", PrintIncomingMessage);
             NetworkComms.AppendGlobalIncomingPacketHandler<ProtocolCl>("Protocol", PrintIncomingMessage);
+            TCPconn.AppendShutdownHandler(disconnect);*/
 
-            Console.WriteLine("Write 'quit' to quit");
+            Console.WriteLine("Write 'quit' to quit | Write 'help' to get available commands");
             while (true)
             {
                 string line = Console.ReadLine();
+
                 if (line == "quit")
                     break;
-                ProtocolCl cmd = new ProtocolCl(Cmd.Ready, Card.None);
-                TCPconn.SendObject("Protocol", cmd);
+                switch (line)
+                {
+                    case "help":
+                        printhelp();
+                        break;
+                    case "BJ":
+                        BlackJack();
+                        break;
+                    case "BT":
+                        Bataille();
+                        break;
+                }
+                //ProtocolCl cmd = new ProtocolCl(Cmd.Ready, Card.None);
+                //TCPconn.SendObject("Protocol", cmd);
             }
-
-            TCPconn.AppendShutdownHandler(disconnect);
+            //TCPconn.AppendShutdownHandler(disconnect);
         }
 
         private static void PrintIncomingMessage(PacketHeader header, Connection connection, string message)
